b05355548b0880861d528d425a6628a6904b8a17
jwMah <wigbuldok@naver.com>
2020-12-27 17:29:23 +0900

    day 3 urls.py URLConf

--- ch3/mysite/urls.py
+++ ch3/mysite/urls.py
@@ -20,4 +20,8 @@ from polls import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('polls/', views.index, name='index'),
+    path('polls/<int:question_id>/', views.detail, name='detail'),
+    path('polls/<int:question_id>/results/', views.results, name='results'),
+    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
 ]
